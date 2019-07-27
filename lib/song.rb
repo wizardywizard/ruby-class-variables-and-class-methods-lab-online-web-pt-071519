ea2b902733d9edbcec40bb4cb49ec99d3260229f
David Little <littledavid175@gmail.com>
2019-07-27 21:32:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,9 @@ class Song
   @@artists = []
   
   def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@genres << genre
     @@artists << artist
@@ -24,13 +27,26 @@ class Song
   end
   
   def self.genre_count
-    if @@genres[genre]
-      @@genres[genre] << @@count
-    else
-      @@genres[genre] = @@count
-    end
+    genre_count = {}
+     @@genres.each do |genre|
+       if genre_count[genre]
+         genre_count[genre] += 1
+       else 
+         genre_count[genre] = 1
+       end
+    end 
+    genre_count
   end
   
   def self.artist_count
+    artist_count = {}
+     @@artists.each do |artist|
+       if artist_count[artist]
+         artist_count[artist] += 1
+       else 
+         artist_count[artist] = 1
+       end
+    end 
+    artist_count
   end
 end
