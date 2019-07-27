85739d46935d40be5945798d0f96cb83d0ab6a87
David Little <littledavid175@gmail.com>
2019-07-27 21:29:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
   end
   
   def self.genre_count
-    genres_count = {}
+    genre_count = {}
      @@genres.each do |genre|
        if genre_count[genre]
          genre_count[genre] += 1
